4a441c65a2889ff892263adfb6b4e269cd9dc4cf
alexis <ajcodalify@gmail.comclear>
2023-03-14 22:31:01 +0800

    "alexis"

--- index.php
+++ index.php
@@ -4,38 +4,36 @@ $page_title = "TailorSys";
 include_once "layout_head.php"; 
 
 
+if (isset($_POST['signin_btn'])) {
+    echo "Signin button is clicked:";
+}elseif(isset($_POST['signup_btn'])){
+    echo "sign up button is clicked:";
+}else{
+    // nothing else
+}
 
 ?>
 
-<!-- signup content here -->
-<div class="hero">
-    <div class="form-box">
-        <div class="button-box">
-            <div id="button"></div>
-                <button type="button" class="toggle-button" onclick="login()">Sign In</button>
-            <button type="button" class="toggle-button" onclick="register()">Register</button>
-        </div>
+<form id="login" class="input-group" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>" method="post">
+    <input type="text" class="input-field" placeholder="Username" required>
+        <input type="password" class="input-field" placeholder="Password" required>
+        <span>Forgot Password? <a href="#" class="forgot_pass">Click Here</a></span>
+        <button type="submit" name="signin_btn" class="submit-button">Log In</button>
+</form>
 
-        <div class="social-icons">
-            <img src="libs/images/fb.png">
-            <img src="libs/images/tw.png">
-            <img src="libs/images/gp.png">
-        </div>
-        <form id="login" class="input-group" action="login_process.php" method="post">
-            <input type="text" class="input-field" placeholder="Username" required>
-            <input type="text" class="input-field" placeholder="Password" required>
-            <input type="checkbox" class="check-box"><span>Remember Password</span>
-            <button type="submit" value="login" class="submit-button">Log In</button>
-        </form>
-        <form id="register" method="post" class="input-group">
-            <input type="text" class="input-field" placeholder="Username" required>
-            <input type="email" class="input-field" placeholder="Email Address" required>
-            <input type="text" class="input-field" placeholder="Password" required>
-            <input type="text" class="input-field" placeholder="Confirm Password" required>
-            <input type="checkbox" class="check-box"><span>I Agree to the Terms & Conditions</span>
-            <button type="submit" value="register" class="submit-button">Register</button>
-        </form>
-    </div>
+
+<form id="register" class="input-group" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>" method="post">
+    <input type="email" class="input-field" placeholder="Email Address" required>
+    <input type="text" class="input-field" placeholder="Name" required>
+    <input type="password" class="input-field" placeholder="Password" required>
+    <input type="password" class="input-field" placeholder="Confirm Password" required>
+    <input type="submit" name="signup_btn" class="submit-button" value="sign up">
+</form>
+
+<div class="social-icons">
+    <img src="libs/images/fb.png">
+    <img src="libs/images/tw.png">
+    <img src="libs/images/gp.png">
 </div>
 
 <?php include_once "layout_foot.php"; ?>
--- layout_head.php
+++ layout_head.php
@@ -8,3 +8,13 @@
     <link rel="stylesheet" href="libs/css/style.css">
 </head>
 <body>
+    <!-- signup content here -->
+<div class="hero">
+
+    <div class="form-box">
+    
+        <div class="button-box">
+            <div id="button"></div>
+                <button type="button" class="toggle-button" onclick="login()">Sign In</button>
+            <button type="button" class="toggle-button" onclick="register()">Sign Up</button>
+        </div>
